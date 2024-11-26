f55694891555194d560bb6e9a8b34355ba5546a5
MateyNedyalkov221889 <112861226+MateyNedyalkov221889@users.noreply.github.com>
2024-11-26 18:58:44 +0100

    Removing the credentials

--- Azure/pipeline_scripts/model_register.py
+++ Azure/pipeline_scripts/model_register.py
@@ -52,12 +52,12 @@ def register_model_if_accuracy_above_threshold(
         print("Model accuracy is above threshold, registering model.")
 
         # Define your Azure ML settings
-        subscription_id = "0a94de80-6d3b-49f2-b3e9-ec5818862801"
-        resource_group = "buas-y2"
-        workspace_name = "CV1"
-        tenant_id = "0a33589b-0036-4fe8-a829-3ed0926af886"
-        client_id = "a2230f31-0fda-428d-8c5c-ec79e91a49f5"
-        client_secret = "Y-q8Q~H63btsUkR7dnmHrUGw2W0gMWjs0MxLKa1C"
+        subscription_id = ""
+        resource_group = ""
+        workspace_name = ""
+        tenant_id = ""
+        client_id = ""
+        client_secret = ""
 
         credential = ClientSecretCredential(tenant_id, client_id, client_secret)
 
